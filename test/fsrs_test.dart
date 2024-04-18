809dddcb3f97e6555d47b5d3275a1c51369a117b
Son Bui <sonbv00@gmail.com>
2024-04-18 11:53:27 +0700

    Clear test file
    Signed-off-by: Son Bui <sonbv00@gmail.com>

--- test/fsrs_test.dart
+++ test/fsrs_test.dart
@@ -4,14 +4,11 @@ import 'package:collection/collection.dart';
 
 void main() {
   group('A group of tests', () {
-    // final awesome = Awesome();
-
     setUp(() {
       // Additional setup goes here.
     });
 
     test('First Test', () {
-      // expect(awesome.isAwesome, isTrue);
       testRepeat();
     });
   });
